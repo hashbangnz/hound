f608580fbf34a2feee0a58a45e8356b71e4d52d3
Arun Agrawal <arunagw@gmail.com>
2015-08-13 16:28:22 +0200

    Use 1.9 syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Houndapp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV.fetch("HOST") }
+  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
