8eb6319f2190db550beb1f09ced2c7bbd202f861
calebbkeene <calebbkeene@gmail.com>
2015-09-08 13:39:27 +1200

    add database.yml and .env to .gitignore, configure app for docker deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,34 @@ preload_app true
 
 timeout 30
 
-listen 3002
+listen 3000
 
 working_directory "/app"
 
 pid "/var/run/unicorn.pid"
 stderr_path "/var/log/unicorn.log"
 stdout_path "/var/log/unicorn.log"
+
+=begin
+before_fork do |server, worker|
+  # Disconnect from db before forking (won't carry over)
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+  end
+end
+
+after_fork do |server, worker|
+  # Start up db connection again
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  if defined?(Resque)
+    Resque.redis = ENV['REDIS_URL']
+  end
+end
+=end
